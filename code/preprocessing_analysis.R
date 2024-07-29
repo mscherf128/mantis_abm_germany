272b035f4f09be985ee038616bdc0209dd6a35ce
mscherf128 <miriamscherf@gmx.net>
2024-07-29 13:47:34 +0200

    english comments

--- code/preprocessing_analysis.R
+++ code/preprocessing_analysis.R
@@ -269,131 +269,103 @@ writeRaster(ST_timecrop_terra_reproject_23, filename = "C:/Users/scher/Documents
                    overwrite = TRUE)
 
 ############################# Statistical analysis #############################
+# Convert data to sf object and transform to match predictors CRS
 mantis_2010_2022_sf <- st_as_sf(mantis_2010_2022, coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
 mantis_2010_2022_sf <- st_transform(mantis_2010_2022_sf, crs(predictors_all))
 
-# Extrahiere die x- und y-Koordinaten aus dem Punktdatensatz
+# Extract coordinates and raster values
 coords <- st_coordinates(mantis_2010_2022_sf)
-
-# Extrahiere Werte aus dem Raster an den Punktpositionen
 mantis_values <- extract(predictors_all, coords)
 
+# Save and load the extracted values
 saveRDS(mantis_values, "C:/Users/scher/Documents/2_Semester/Fernerkundung/mantis_abm_germany/data/mantis_values")
 mantis_values <- readRDS("C:/Users/scher/Documents/2_Semester/Fernerkundung/mantis_abm_germany/data/mantis_values")
 
 ############################## landcover class 2018 ############################
+# Extract and save/load land cover class values for 2018
 LC_18_gottan <- mantis_values[ , "LABEL3"]
 saveRDS(LC_18_gottan, "C:/Users/scher/Documents/2_Semester/Fernerkundung/mantis_abm_germany/data/LC_18_gottan")
 LC_18_gottan <- readRDS("C:/Users/scher/Documents/2_Semester/Fernerkundung/mantis_abm_germany/data/LC_18_gottan")
 
+# Calculate and filter land cover class counts
 LC_counts <- table(LC_18_gottan)
-
-# Landnutzungsklassen mit einer Häufigkeit von über 100 filtern
 LC_100 <- LC_counts[LC_counts > 100]
-# Namen der Landnutzungsklassen mit einer Häufigkeit von über 100
 names(LC_100)
-
-# Landnutzungsklassen mit einer Häufigkeit von über 200 filtern
 LC_200 <- LC_counts[LC_counts > 200]
-# Namen der Landnutzungsklassen mit einer Häufigkeit von über 200
 names(LC_200)
-
-# Landnutzungsklassen mit einer Häufigkeit von über 300 filtern
 LC_300 <- LC_counts[LC_counts > 300]
-# Namen der Landnutzungsklassen mit einer Häufigkeit von über 300
 names(LC_300)
-
-# Landnutzungsklassen mit einer Häufigkeit von über 350 filtern
 LC_350 <- LC_counts[LC_counts > 350]
-# Namen der Landnutzungsklassen mit einer Häufigkeit von über 350
 names(LC_350)
 
-#Read Corine classes
+# Read Corine classes
 clc_classes <- foreign::read.dbf("C:/Users/scher/Documents/2_Semester/Fernerkundung/mantis_abm_germany/data/CORINE_2018.dbf",
-                                 as.is = TRUE) %>%dplyr::select(value = Code_18,landcov = LABEL3)
+                                 as.is = TRUE) %>% dplyr::select(value = Code_18, landcov = LABEL3)
 
-# Zusammenfassung der Daten
+# Summarize and calculate frequencies
 summary(LC_18_gottan)
-
-# Häufigkeiten berechnen
 freq_table <- table(LC_18_gottan)
 cat("Absolut Häufigkeit:\n")
 print(freq_table)
-
 relative_freq <- prop.table(freq_table)
 cat("\nRelative Häufigkeit:\n")
 print(relative_freq)
 
-# Häufigkeitstabelle erstellen
+# Create frequency table and plot bar chart
 freq_table_df <- as.data.frame(freq_table)
 names(freq_table_df) <- c("Kategorie", "Häufigkeit")
 freq_table_df$Relative_Häufigkeit <- relative_freq
 print("\nHäufigkeitstabelle:")
 print(freq_table_df)
 
-# Balkendiagramm für Häufigkeiten
-par(mar = c(3, 20, 3, 2) + 0.1) # Rand einstellen: unten, links, oben, rechts
-
-barplot(freq_table, 
-        #xlab = "Kategorie",
-        #ylab = "frequency",
-        col = "grey",
-        las = 1, cex.names = 0.58,
-        horiz = TRUE,
-        xlim = c(0, 400))  # las = 1 dreht die Beschriftungen um 90 Grad
-
-# Anpassung der Position des xlab
-mtext("frequency", side = 1, line = 2, cex = 0.8)  # line anpassen je nach Bedarf
-
-# Anpassung der Position des ylab
+par(mar = c(3, 20, 3, 2) + 0.1) # Set margins
+barplot(freq_table, col = "grey", las = 1, cex.names = 0.58, horiz = TRUE, xlim = c(0, 400))
+mtext("frequency", side = 1, line = 2, cex = 0.8)
 mtext("land cover class", side = 2, line = 0, cex = 0.8)
-
-# Horizontale Linien einfügen
-abline(v = 100, col = "green", lwd = 2, lty = 2)  # Höhe 100, grün, Breite 2, gestrichelt
-abline(v = 200, col = "blue", lwd = 2, lty = 2)  # Höhe 200, blau, Breite 2, gestrichelt
-abline(v = 300, col = "purple", lwd = 2, lty = 2)  # Höhe 300, lila, Breite 2, gestrichelt
-abline(v = 350, col = "red", lwd = 2, lty = 2)  # Höhe 350, rot, Breite 2, gestrichelt
+abline(v = 100, col = "green", lwd = 2, lty = 2)
+abline(v = 200, col = "blue", lwd = 2, lty = 2)
+abline(v = 300, col = "purple", lwd = 2, lty = 2)
+abline(v = 350, col = "red", lwd = 2, lty = 2)
 
 ########################### landcover impervious built-up ######################
+# Extract and save/load impervious built-up values
 LC_IB_gottan <- mantis_values[ , "IBU_2018_010m_03035_V1_0"]
 saveRDS(LC_IB_gottan, "C:/Users/scher/Documents/2_Semester/Fernerkundung/mantis_abm_germany/data/LC_IB_gottan")
 LC_IB_gottan <- readRDS("C:/Users/scher/Documents/2_Semester/Fernerkundung/mantis_abm_germany/data/LC_IB_gottan")
 
-# Zusammenfassung der Daten
+# Summarize and calculate statistics
 summary(LC_IB_gottan)
 mean_value <- mean(LC_IB_gottan)
 median_value <- median(LC_IB_gottan)
 variance_value <- var(LC_IB_gottan)
 sd_value <- sd(LC_IB_gottan)
 freq_table <- table(LC_IB_gottan)
-
 cat("Mittelwert:", mean_value, "\n")
 cat("Median:", median_value, "\n")
 cat("Varianz:", variance_value, "\n")
 cat("Standardabweichung:", sd_value, "\n")
 print(freq_table)
 
-# Relative Häufigkeiten berechnen
+# Calculate relative frequencies and create frequency table
 relative_freq <- prop.table(freq_table)
 print(relative_freq)
-
-# Häufigkeitstabelle
 freq_table_df <- as.data.frame(freq_table)
 names(freq_table_df) <- c("Wert", "Häufigkeit")
 freq_table_df$Relative_Häufigkeit <- relative_freq
 print(freq_table_df)
 
-# Balkendiagramm für Häufigkeiten
+# Plot bar chart for frequencies
 par(mar = c(5.1, 4.1, 4.1, 2.1))
 barplot(freq_table, main = "Frequency of impervious built-up values\nat the mantis observation points",
         ylab = "frequency", col = "grey",
         names.arg = c("impervious built-up absent", "impervious built-up present"))
 
 ############################### elevation ######################################
+# Extract elevation values and remove NAs
 elev_gottan <- mantis_values[ , "DEU_elv_msk"]
 elev_gottan <- na.omit(elev_gottan)
 
-# Statistische Auswertung
+# Summarize and calculate statistics
 mean_elev <- mean(elev_gottan)
 sd_elev <- sd(elev_gottan)
 median_elev <- median(elev_gottan)
@@ -401,8 +373,6 @@ min_elev <- min(elev_gottan)
 max_elev <- max(elev_gottan)
 q1_elev <- quantile(elev_gottan, 0.25)
 q3_elev <- quantile(elev_gottan, 0.75)
-
-# Ausgabe der Statistiken
 cat("Statistische Auswertung von elev_gottan:\n")
 cat("mean:", mean_elev, "\n")
 cat("standard deviation:", sd_elev, "\n")
@@ -412,40 +382,29 @@ cat("maximum:", max_elev, "\n")
 cat("1. Quartil (Q1):", q1_elev, "\n")
 cat("3. Quartil (Q3):", q3_elev, "\n")
 
-# Boxplot zur Darstellung der Verteilung
+# Plot boxplot for elevation values
 boxplot(elev_gottan, main = "Boxplot of elevation values at the\nmantis observation points ", ylab = "elevation in m")
 
 ############################ surface temperature ###############################
-surftemp_gottan <- mantis_values[ , 4:159]
-# von Kelvin in °C umrechnen
-surftemp_gottan <- surftemp_gottan - 273.15
+# Extract surface temperature values and convert from Kelvin to Celsius
+surftemp_gottan <- mantis_values[ , 4:159] - 273.15
 summary(surftemp_gottan)
 
-# Zuerst die Spaltennamen extrahieren
+# Select May data for the years 2010-2022
 column_names <- colnames(surftemp_gottan)
-
-# Mai-Daten der Jahre auswählen: Frühjahrstemperaturen sind entscheidend für die Entwicklung der Mantis
-# Die Spaltennamen filtern, die den Monat März und die Jahre 2010-2022 enthalten
 may_columns <- grep("time20(1[0-9]|2[0-2])-05", column_names, value = TRUE)
-
-# Die gefilterten Spalten aus dem Datensatz extrahieren
 may_data <- surftemp_gottan[, may_columns]
 
-# Minima für jede Spalte berechnen
+# Calculate minima and means for May data
 min_may_values <- apply(may_data, 2, min, na.rm = TRUE)
-
-# Mittelwerte für jede Spalte berechnen
 mean_may_values <- apply(may_data, 2, mean, na.rm = TRUE)
-
-# Jahre aus den Spaltennamen extrahieren
 years_may <- sub("time(\\d{4})-05.*", "\\1", colnames(may_data))
 
-# Minima und Mittelwerte in DataFrames zusammenführen
+# Create dataframes for minima and means
 min_may_data <- data.frame(Year = as.numeric(years_may), Min = min_may_values)
 mean_may_data <- data.frame(Year = as.numeric(years_may), Mean = mean_may_values)
 
-# Diagramme erstellen
-# Minima in einem Diagramm darstellen
+# Plot minima and means
 ggplot(min_may_data, aes(x = Year, y = Min)) +
   geom_line() +
   geom_point() +
@@ -454,7 +413,6 @@ ggplot(min_may_data, aes(x = Year, y = Min)) +
        y = "Minimum") +
   theme_minimal()
 
-# Mittelwerte in einem Diagramm darstellen
 ggplot(mean_may_data, aes(x = Year, y = Mean)) +
   geom_line() +
   geom_point() +
@@ -463,16 +421,15 @@ ggplot(mean_may_data, aes(x = Year, y = Mean)) +
        y = "mean temperature [°C]") +
   scale_x_continuous(breaks = seq(min(mean_may_data$Year), max(mean_may_data$Year), by = 1), minor_breaks = NULL) +
   scale_y_continuous(breaks = seq(floor(min(mean_may_data$Mean)), ceiling(max(mean_may_data$Mean)), by = 1)) +
-  theme_minimal()+
+  theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5))
 
-# Datenrahmen für den Boxplot erstellen
+# Create dataframe and boxplot for mean May surface temperature
 mean_mai_df <- data.frame(
   Year = rep("", length(mean_may_values)),
   Mean = mean_may_values
 )
 
-# Boxplot für die Mittelwerte der Oberflächentemperatur im Mai erstellen
 ggplot(mean_mai_df, aes(x = Year, y = Mean)) +
   geom_boxplot() +
   scale_y_continuous(breaks = seq(floor(min(mean_mai_df$Mean)), ceiling(max(mean_mai_df$Mean)), by = 1)) +
@@ -481,4 +438,3 @@ ggplot(mean_mai_df, aes(x = Year, y = Mean)) +
        y = "mean surface temperature [°C]") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5))
-
--- code/validation_for_2023.R
+++ code/validation_for_2023.R
@@ -1,35 +1,47 @@
+# Clear the workspace
 rm(list=ls())
 getwd()
 
 ################################# Packages #####################################
+# Load the mapview package for interactive mapping
 library(mapview)
 
-# Data 2022
+# Load the 2022 data
 gottan_2022_sf <- readRDS("C:/Users/scher/Documents/2_Semester/Fernerkundung/mantis_abm_germany/data/mantis_2022")
 
-# observed Data
+# Load the observed 2023 data
 gottan_2023_obs <- readRDS("C:/Users/scher/Documents/2_Semester/Fernerkundung/mantis_abm_germany/data/mantis_2023")
 
-# predicted Data
+# Load the predicted 2023 data from a shapefile
 gottan_2023_pred <- st_read("C:/Users/scher/Documents/2_Semester/Fernerkundung/mantis_abm_germany/data/mantis_2023_modeled.shp")
+# Set the coordinate reference system (CRS) for the predicted data
 st_crs(gottan_2023_pred) <- st_crs(32632)
+# Visualize the predicted data
 mapview(gottan_2023_pred)
 
+# Extract the geometry of the predicted data
 gottan_2023_predicted <- gottan_2023_pred["geometry"]
+# Convert the extracted geometry to an sf object with specified CRS
 gottan_2023_predicted_sf <- st_as_sf(gottan_2023_predicted, coords = coordinates, crs = 4326)
 
+# Set the CRS for the observed data
 st_crs(gottan_2023_obs) <- st_crs(4326)
 
 # Nearest neighbour
+# Transform the observed data to match the CRS of the predicted data
 gottan_2023_obs_proj <- st_transform(gottan_2023_obs, st_crs(gottan_2023_predicted))
+# Calculate the nearest neighbor distances between observed and predicted points
 nndist <- FNN::knnx.dist(data = st_coordinates(gottan_2023_obs_proj), query = st_coordinates(gottan_2023_predicted))
 
+# Create a dataframe to store the distances and their mean
 distances_df_for_2023_pred <- data.frame(nndist, mean=rowMeans(nndist))
+# Add the mean nearest neighbor distance to the predicted data
 gottan_2023_predicted$mean_10_nnd <- distances_df_for_2023_pred$mean
 
-# Abbildungen results
+# Plot a histogram of the mean nearest neighbor distances
 hist(gottan_2023_predicted$mean_10_nnd, breaks=50)
 
+# Plot the empirical cumulative distribution function (ECDF) of the mean nearest neighbor distances
 plot(ecdf(gottan_2023_predicted$mean_10_nnd),
      do.points = FALSE,
      main = "Empirical cumulative distribution function of mean NND",
@@ -38,25 +50,28 @@ plot(ecdf(gottan_2023_predicted$mean_10_nnd),
      xaxt = "n")
 abline(h = 0.8, col = "red", lwd = 2, lty = 2)
 
+# Create the ECDF function for the mean nearest neighbor distances
 ecdf_function <- ecdf(gottan_2023_predicted$mean_10_nnd)
 
-# Bestimme den x-Wert, der dem y-Wert 0.8 entspricht
-# Finde den kleinsten x-Wert, für den die ecdf >= 0.8 ist
+# Determine the x-value corresponding to the y-value of 0.8 in the ECDF
 x_value <- min(gottan_2023_predicted$mean_10_nnd[ecdf_function(gottan_2023_predicted$mean_10_nnd) >= 0.8])
 x_value
 
+# Add a vertical line at the x-value
 abline(v = x_value, col = "black", lwd = 2, lty = 2)
 
-# Drehung der Beschriftung der x-Achse
-par(las = 2) # las = 2 dreht die Achsenbeschriftung vertikal
+# Rotate the x-axis labels
+par(las = 2) # las = 2 rotates the axis labels vertically
 axis(1, at = seq(0, max(gottan_2023_predicted$mean_10_nnd), by = 10000))
-par(las = 1) # Zurücksetzen der Achsenbeschriftung zur Standardausrichtung
+par(las = 1) # Reset the axis labels to the default orientation
 
-# Füge das x-Achsen-Label mit einem größeren Abstand hinzu
-mtext("mean NND [m]", side = 1, line = 4)  # line = 4 legt den Abstand fest, je größer der Wert, desto weiter unten
+# Add a label to the x-axis with a larger distance from the axis
+mtext("mean NND [m]", side = 1, line = 4)  # line = 4 sets the distance, the larger the value, the further down
 
+# Load administrative boundaries for Germany
 DEgadmsf <- readRDS("C:/Users/scher/Documents/2_Semester/Fernerkundung/mantis_abm_germany/data/DEgadmsf")
 
+# Plot the observed and predicted mantis occurrences for 2023 on a map of Germany
 ggplot() +
   geom_sf(data = DEgadmsf, fill = "snow2") +
   geom_sf(data = gottan_2023_obs_proj, aes(color = "mantis occurrence 2023, observed"), size = 2, pch = 20) +
@@ -64,42 +79,42 @@ ggplot() +
   scale_color_manual(name = "", 
                      values = c("mantis occurrence 2023, observed" = "blue", "mantis occurrence 2023, predicted" = "red"),
                      labels = c("mantis occurrence\n2023, observed", "mantis occurrence\n2023, predicted")) +
-  theme_minimal()+
+  theme_minimal() +
   theme(
-    axis.text = element_text(size = 12),       # Schriftgröße der Achsenbeschriftungen
-    axis.title = element_text(size = 14),      # Schriftgröße der Achsentitel
-    legend.text = element_text(size = 11),     # Schriftgröße des Legendentextes
-    legend.title = element_text(size = 12)     # Schriftgröße des Legendentitels
+    axis.text = element_text(size = 12),       # Font size for axis labels
+    axis.title = element_text(size = 14),      # Font size for axis titles
+    legend.text = element_text(size = 11),     # Font size for legend text
+    legend.title = element_text(size = 12)     # Font size for legend title
   )
 
+# Plot the observed mantis occurrences for 2022 on a map of Germany
 ggplot() +
   geom_sf(data = DEgadmsf, fill = "lightyellow") +
   geom_sf(data = gottan_2022_sf, aes(color = "mantis occurrence 2022, observed"), size = 2, pch = 20) +
   scale_color_manual(name = "", 
                      values = c("mantis occurrence 2022, observed" = "darkgrey"),
                      labels = c("mantis occurrence\n2022, observed")) +
-  theme_minimal()+
+  theme_minimal() +
   theme(
-    axis.text = element_text(size = 12),       # Schriftgröße der Achsenbeschriftungen
-    axis.title = element_text(size = 14),      # Schriftgröße der Achsentitel
-    legend.text = element_text(size = 11),     # Schriftgröße des Legendentextes
-    legend.title = element_text(size = 12)     # Schriftgröße des Legendentitels
+    axis.text = element_text(size = 12),       # Font size for axis labels
+    axis.title = element_text(size = 14),      # Font size for axis titles
+    legend.text = element_text(size = 11),     # Font size for legend text
+    legend.title = element_text(size = 12)     # Font size for legend title
   )
 
+# Plot the predicted mantis occurrences for 2023 on a map of Germany, colored by mean nearest neighbor distance
 ggplot() +
   geom_sf(data = DEgadmsf, fill = "snow2") +
   geom_sf(data = gottan_2023_predicted, aes(color = mean_10_nnd), size = 2, pch = 20) +
   scale_color_viridis_c(name = "mean 10 NND [meter]",
                         breaks = c(20000, 40000, 60000, 80000),
-                        labels = c("20.000","40.000", "60.000","80.000"),
+                        labels = c("20,000","40,000", "60,000","80,000"),
                         begin = 0,
-                        end = 1,
-                        #trans = "log"
-  ) +
-  theme_minimal()+
+                        end = 1) +
+  theme_minimal() +
   theme(
-    axis.text = element_text(size = 12),       # Schriftgröße der Achsenbeschriftungen
-    axis.title = element_text(size = 14),      # Schriftgröße der Achsentitel
-    legend.text = element_text(size = 11),     # Schriftgröße des Legendentextes
-    legend.title = element_text(size = 12)     # Schriftgröße des Legendentitels
+    axis.text = element_text(size = 12),       # Font size for axis labels
+    axis.title = element_text(size = 14),      # Font size for axis titles
+    legend.text = element_text(size = 11),     # Font size for legend text
+    legend.title = element_text(size = 12)     # Font size for legend title
   )
